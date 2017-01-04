74942a2b4f5eb32055c87070a6c3c93dc691de15
nombumurage <nombumurage@gmail.com>
2017-01-04 17:15:02 +0300

    2017-01-04

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/pingpong-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
